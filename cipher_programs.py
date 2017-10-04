b18692bfcb697ae13b400c105642c5f4f825f5a2
Jesse Francispillai <jesse.nf30@gmail.com>
2017-10-04 13:44:14 -0400

    Update cipher_programs.py

--- cipher_programs.py
+++ cipher_programs.py
@@ -19,28 +19,16 @@ def main():
 
     # Create the file handle for opening the deck file
     # Then set the deck_cards list by calling read_deck function
-    deck_file_handle = open(DECK_FILENAME, "r")
-    deck_cards = cipher_functions.read_deck(deck_file_handle)
     # Create the file handle for opening the message file
     # Then set the messages list by calling read_messages function
-    message_file_handle = open(MSG_FILENAME)
-    messages = cipher_functions.read_messages(message_file_handle)
-
+    
     # If MODE is 'e' then encrypt, if not then decrypt
-    if(MODE == 'e'):
-        action = 'e'
-    else:
-        action = 'd'
+    
 
     # Create a list to store the encrypted/decrypted messages. This uses the
     # main function: process_messages
-    output_list = cipher_functions.process_messages(deck_cards,
-                                                    messages, action)
+    
     # Go through each message in the list to print on a new line
-    for element in output_list:
-        print(element)
+    
     # Close the file handles
-    deck_file_handle.close()
-    message_file_handle.close()
-
-    pass
+   
